152421c4ef8bfbfe1d8b0004e9f6bb50e4c6d2ce
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2019-03-29 12:11:49 +0800

    Initial test
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- tests/Unit/SampleTest.php
+++ tests/Unit/SampleTest.php
@@ -8,6 +8,7 @@
 
 namespace HalcyonLaravel\AuditHistory\Tests\Unit;
 
+use Exception;
 use HalcyonLaravel\AuditHistory\Tests\TestCase;
 
 class SampleTest extends TestCase
@@ -15,12 +16,17 @@ class SampleTest extends TestCase
     /**
      * @test
      */
-    public function sample()
+    public function invalid_arg()
     {
-        $this->actingAs($this->user);
-        $this->testModel->update([
-            'first_name' => 'new name',
-        ]);
-        $this->assertEquals('test', app('audit-history')->buildClass(get_class($this->testModel))->render(10));
+        $this->expectException(Exception::class);
+        $this->assertEquals('test', app('audit-history')->buildClass(get_class($this->user))->render(10));
+    }
+
+    /**
+     * @test
+     */
+    public function valid()
+    {
+        $this->assertNotNull(app('audit-history')->buildClass(get_class($this->testModel))->render(10));
     }
 }
